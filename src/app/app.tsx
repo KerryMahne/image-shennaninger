24c89ff3a67fdb28e67248596ab07c75f088caa7
Kerry Mahne <kerry.mahne@gmail.com>
2022-06-02 15:17:26 +0200

    feat(Theme): use styled components for global theme

---
+++ src/app/app.tsx
@@ -0,0 +1,25 @@
+import '../App.css';
+import { GlobalTheme } from '../theme/global_theme';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      <GlobalTheme />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/theme/global_theme.ts
@@ -0,0 +1,66 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalTheme = createGlobalStyle`
+  :root {
+    --color-primary: #004bff;
+    --color-secondary: #00fac8;
+
+    --color-text-primary: #0f141e;
+    --color-text-secondary: #474c56;
+
+    --color-background-primary: #fff;
+    --color-background-secondary: #e5e5e5;
+
+    --color-border-primary: #646466;
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+
+  html, body, #root {
+    margin: 0;
+    padding: 0;
+    height: 100%;
+  }
+
+  html {
+    font-size: 62.5%; /* 10px === 1rem */
+  }
+
+  body {
+    font-family: Archia, sans-serif;
+    font-size: 1.6rem;
+    line-height: 1.6;
+    color: var(--color-text-primary);
+    background-color: var(--color-background-primary);
+  }
+
+  h1, h2, h3 {
+    font-family: Archia, sans-serif;
+    font-weight: 800;
+    margin: 3rem 0;
+  }
+
+  h1 {
+    font-size: 3.6rem;
+  }
+
+  h2 {
+    font-size: 2.8rem;
+  }
+
+  h3 {
+    font-size: 2.4rem;
+  }
+
+  p {
+    font-size: 1.6rem;
+    margin: 3rem 0;
+  }
+
+  a {
+    color: var(--special-link);
+    text-decoration: underline;
+  }
+`;
