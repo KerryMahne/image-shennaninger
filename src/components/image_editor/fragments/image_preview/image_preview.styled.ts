8dba692a1f4e19f5bf17973f0e745235d5046f87
Kerry Mahne <kerry.mahne@gmail.com>
2022-06-05 17:09:13 +0200

    fix: crop image from the left instead of center

--- src/components/image_editor/fragments/image_preview/image_preview.styled.ts
+++ src/components/image_editor/fragments/image_preview/image_preview.styled.ts
@@ -4,6 +4,7 @@ import { ImageControls } from '../../image_editor.interfaces'
 export const Image = styled.img<ImageControls>`
   ${({ width, height, grayscale, blur }) => `
   object-fit: cover;
+  object-position: 0 0;
 
   width: ${width}px;
   height: ${height}px;
